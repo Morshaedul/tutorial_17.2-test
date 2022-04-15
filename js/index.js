cc0f6fa85c5a4f9b633c4987ce68769841f0c4bd
Morshaedul <morshaedulanirbanit@gmail.com>
2022-04-16 00:15:28 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+
+document.write(" <h3> Name: Morshaedul Alom </h3> </br>" );
+document.write(" <h3> Father's Name: Yeaqub Ali </h3> </br>");
+document.write(" <h3> Mother's Name: Shahera Banu </h3> </br>");
+document.write(" <h3> Mobile No: 01774662814 </h3> </br>");
+console.log("My name is Morshaedul alom");
+console.log("my girlfried name is Sabrina sultana");
+console.log('Sabrina sultana');
+document.getElementById('demo').innerHTML='SABRINA';
+
+function Sabrina(a,b){
+    c=a+b;
+    return c;
+}
+let v=Sabrina(20,10);
+console.log('Sum='+v);
+
+let i;
+for(i=1; i<10; i++){
+    document.write(i);
+}
